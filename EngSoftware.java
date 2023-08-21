2338d2f9ed0a8945e5fe1768c0d1ba5e545bfedc
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-08-21 08:14:08 -0300

    Classe  EngSoftware.java

---
+++ EngSoftware.java
@@ -0,0 +1,31 @@
+package ex210823;
+
+public class EngSoftware extends Profissao{
+
+   private boolean hardware, software, rede;
+    
+    
+    public boolean isHardware() {
+        return hardware;
+    }
+
+    public void setHardware(boolean hardware) {
+        this.hardware = hardware;
+    }
+
+    public boolean isSoftware() {
+        return software;
+    }
+
+    public void setSoftware(boolean software) {
+        this.software = software;
+    }
+
+    public boolean isRede() {
+        return rede;
+    }
+
+    public void setRede(boolean rede) {
+        this.rede = rede;
+    }    
+}
